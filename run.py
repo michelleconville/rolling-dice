f41bdffa8cf81547664e1d4a772ae881a5471578
MaksimB96 <maksims.buraks.1996@gmail.com>
2023-01-12 19:52:34 +0000

    intro

--- run.py
+++ run.py
@@ -1,3 +1,14 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+"""
+Libraries 
+"""
+
+import random
+from graphicstuff import welcome
+
+def intro_screen():
+    """
+    intro strings
+    """
+
+    print(welcome)
+   
