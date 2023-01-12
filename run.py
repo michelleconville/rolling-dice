31d62a1fa471dedee7cca49d5b1ad891e6d98188
MaksimB96 <maksims.buraks.1996@gmail.com>
2023-01-12 20:12:41 +0000

    commit 3

--- run.py
+++ run.py
@@ -5,19 +5,27 @@ Libraries
 import random
 from graphicstuff import welcome
 
-di_input = input("How many dice do you want to roll? [1-3]\n")
-di_num = parse_input(di_input)
-
-def parse_input(input_string):
-    if input_string.strip() in {"1", "2", "3"}:
-        return int(input_string)
-    else:
-
 def intro_screen():
     """
     intro strings
     """
     print(welcome)
+
+intro_screen()
+
+def parse_input(input_string):
+    if input_string.strip() in {"1", "2", "3"}:
+        return int(input_string)
+    else:
+        print("Please enter a number from 1 and 3")
+        raise SystemExit(1)
+
+di_input = input("How many dice do you want to roll? [1-3]\n")
+di_num = parse_input(di_input)
+
+
+
+
 
 
 
